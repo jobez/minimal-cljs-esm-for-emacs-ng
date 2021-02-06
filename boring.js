c4d14e0d75b29325f8fcc1a49bf7239f89aa4ef2
David DeSimone <lord.good.mail@gmail.com>
2021-02-05 18:10:16 -0800

    Updating example so that users can just 'run' eval-js-buffer

--- boring.js
+++ boring.js
@@ -1,12 +1,6 @@
+let path =  lisp.buffer_file_name().split('/');
+path.pop();
 
-
-let timestamp = Date.now();
-
-let path = "put result of M-x pwd here";
-
-const { foo } = await import(`${path}/target/baz.js#${timestamp}`);
-
-const buffer = lisp.get_buffer_create("*Org JSON Export*");
-
-lisp.with_current_buffer(buffer, () => lisp.print(foo("wow, what a boring minimal example how dare you", lisp)));
+const { foo } = await import(`${path.join('/')}/target/baz.js#${Date.now()}`);
+foo("wow, what a boring minimal example how dare you", lisp);
 
