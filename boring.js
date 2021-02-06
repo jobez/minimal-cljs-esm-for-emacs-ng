85baee15699ad56b768587f926d89b41cea25a64
David DeSimone <lord.good.mail@gmail.com>
2021-02-05 21:30:46 -0800

    Fixing auto-refresh for a better dev environment

--- boring.js
+++ boring.js
@@ -1,5 +1,51 @@
+const autogen = true; // Set this to false to prevent
+                      // auto-refresh
+
+const refresh = async (filename, ts) => {
+    let text = await Deno.readTextFile(filename);
+    let now = Date.now();
+    let lines = text.split('\n');
+    for (const line of lines) {
+	if (line.includes('goog.')
+	    || line.includes('cljs.')
+	    || line.includes('clojure.')) {
+	    continue;
+	}
+
+	text = text.replace(line, line.replace(`.js";`, `.js#${now}";`));
+    }
+    await Deno.writeTextFile(filename, text);
+};
+
+const cljsImport = async (fn) => {
+    const fileInfo = await Deno.lstat(fn);
+    const time = fileInfo.mtime;
+    if (autogen) {
+	await refresh(fn, time);
+    }
+
+    const filename = `${fn}#${time}`;
+    let result = await import(filename);
+    // A little bit of a hack.
+    let oldProvide = goog.provide;
+    goog.provide = (...args) => {
+	try {
+	    oldProvide(...args);
+	    goog.provide = oldProvide;
+	} catch (e) {
+	    // Drop it on purpose...
+	}
+    };
+
+    return result;
+}
+
 let path =  lisp.buffer_file_name().split('/');
 path.pop();
-
-const { cljsprint } = await import(`${path.join('/')}/target/baz.js#${Date.now()}`);
+const base = `${path.join('/')}/target`;
+const fn = `${base}/baz.js`;
+const { cljsprint, addl, addx } = await cljsImport(fn);
 cljsprint("wow, what a boring minimal example how dare you");
+lisp.print(addx(5, 20))
+
+
--- src/app/main.cljs
+++ src/app/main.cljs
@@ -2,3 +2,10 @@
 
 (defn bar [message]
   (.message js/lisp message))
+
+
+(defn add3 [n]
+  (+ n 30))
+
+(defn addx [n m]
+      (+ n m))
